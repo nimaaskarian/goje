77ff5adf0b03ecc0f637967ee2250587e73b704b
nimaaskarian <nimaaskarian.work@outlook.com>
2025-05-30 11:35:19 +0330

    Run daemonCmd by default

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: coverage.out goje
+all: coverage.out goje goje.exe
 SRC = $(shell find -name "*.go" -type f)
 WEBGUI = httpd/webgui-preact/dist
 WEBGUI_DEPS = $(wildcard httpd/webgui-preact/src/* httpd/webgui-preact/public/* httpd/webgui-preact/index.html)
@@ -8,6 +8,9 @@ coverage.out: $(SRC)
 
 goje: ${WEBGUI} $(SRC)
 	go build
+
+goje.exe: ${WEBGUI} $(SRC)
+	GOOS=windows go build
 
 ${WEBGUI}: ${TW} $(WEBGUI_DEPS)
 	cd httpd/webgui-preact/; npm run build
--- cmd/root.go
+++ cmd/root.go
@@ -5,17 +5,23 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "goje",
 	Short: "a server-client pomodoro timer",
-	Long: "goje is a mpd-like, client server pomodoro timer with modern features",
+	Long:  "goje is a mpd-like, client server pomodoro timer with modern features",
 }
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	cmd, _, err := rootCmd.Find(os.Args[1:])
+	if err == nil && cmd.Use == rootCmd.Use && cmd.Flags().Parse(os.Args[1:]) != pflag.ErrHelp {
+		args := append([]string{daemonCmd.Use}, os.Args[1:]...)
+		rootCmd.SetArgs(args)
+	}
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
